fcb31f83b3e100b5de144afcc2c25f8437f52f26
Chernyaev Aleksey <chernyaevaleksey2018@gmail.com>
2026-06-07 20:00:31 +0300

    Update Board.h

--- Source/Public/Board.h
+++ Source/Public/Board.h
@@ -20,37 +20,25 @@ public:
   Board();
 
   void Draw(sf::RenderWindow &window, const std::optional<sf::Vector2i> &selected);
-
   bool Swap(int x1, int y1, int x2, int y2);
-
   void Update(float dt);
-
   bool IsInside(int x, int y) const;
 
 private:
   std::vector<std::vector<std::optional<Gem>>> m_Grid;
-
   std::mt19937 m_Rng;
-
   bool m_Animating = false;
 
 private:
   void Generate();
-
   Gem RandomGem();
-
   bool FindMatches();
   void RemoveMatches();
-
   void ApplyGravity();
-
   void FillEmpty();
-
   void SpawnRandomBonus(int x, int y, GemColor sourceColor);
-
   void ApplyBomb(int x, int y);
   void ApplyRecolor(int x, int y, GemColor color);
-
   void StartDestroy();
   bool HasDestroyAnimation() const;
   void RemoveDestroyed();
