aebcc48e3e6f258cb8d53409cdabdb4231df5188
Chernyaev Aleksey <chernyaevaleksey2018@gmail.com>
2026-06-07 20:00:00 +0300

    Update Game.cpp

--- Source/Private/Game.cpp
+++ Source/Private/Game.cpp
@@ -1,30 +1,21 @@
 #include "Game.h"
 
 Game::Game()
-    : m_Window(sf::VideoMode({Board::Width * Board::TileSize,
-                              Board::Height * Board::TileSize}),
-               "GEMS") {}
+    : m_Window(sf::VideoMode({Board::Width * Board::TileSize, Board::Height * Board::TileSize}), "GEMS") {}
 
-void Game::Run() {
+void Game::Run() { //запуск
   sf::Clock clock;
-
   while (m_Window.isOpen()) {
-
     float dt = clock.restart().asSeconds();
-
     ProcessEvents();
-
     m_Board.Update(dt);
-
     Render();
   }
 }
 
-void Game::ProcessEvents() {
+void Game::ProcessEvents() { 
   while (const std::optional event = m_Window.pollEvent()) {
-    if (event->is<sf::Event::Closed>()) {
-      m_Window.close();
-    }
+    if (event->is<sf::Event::Closed>()) { m_Window.close(); }
 
     if (const auto* mouseEvent = event->getIf<sf::Event::MouseButtonPressed>()) {
       int x = mouseEvent->position.x / Board::TileSize;
@@ -32,13 +23,9 @@ void Game::ProcessEvents() {
       int y = mouseEvent->position.y / Board::TileSize;
 
       if (!m_Selected.has_value()) {
-
         m_Selected = sf::Vector2i(x, y);
-
       } else {
-
         m_Board.Swap(m_Selected->x, m_Selected->y, x, y);
-
         m_Selected.reset();
       }
     }
@@ -47,8 +34,6 @@ void Game::ProcessEvents() {
 
 void Game::Render() {
   m_Window.clear(sf::Color::Black);
-
   m_Board.Draw(m_Window, m_Selected);
-
   m_Window.display();
 }
